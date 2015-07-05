be7f850a2ef9fdf73bcd21cf5083fa0c8c6c7378
Kamil Kiełbasa <kamil.m.kielbasa@gmail.com>
2015-07-05 18:49:00 +0200

    more ui

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var usage = require('./routes/usage');
 var users = require('./routes/users');
 var xml = require('./routes/xml');
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/usage', usage);
 app.use('/xml', xml);
 
 // catch 404 and forward to error handler
